62b0f4108b5e6ccb7acead00500f2ccc52b26532
EmilJK <emjoka18@gmail.com>
2020-02-28 14:18:06 +0100

    Initializes Unity
    Opprettelse av prosjektet, byttet til Android, og opprettelse av scripts som skifter farge på en boks.

---
+++ Assets/Scripts/CubeColorRandom.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeColorRandom : MonoBehaviour
+{
+    public void OnBecameInvisible()
+    {
+        GetComponent<MeshRenderer>().material.color = Random.ColorHSV();
+    }
+}
---
+++ Assets/Scripts/NativeTouch.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NativeTouch : MonoBehaviour
+{
+    
+    void Update()
+    {
+        if(Input.touchCount > 0)
+        {
+            Touch touch = Input.touches[0];
+            print(touch.position);
+            if(touch.phase == TouchPhase.Began)
+            {
+                //When the touch begins, stuff will happen
+            }
+            else if(touch.phase == TouchPhase.Ended)
+            {
+                //When the touch is ended, something will happen
+            }
+
+            Vector3 scenePos;
+            scenePos = Camera.main.ScreenToViewportPoint(touch.position);
+        }
+    }
+}
